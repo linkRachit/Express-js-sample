08518fe33068b58f23f41f80893930fcdf065730
Rachit Goel <rachit2goel@gmail.com>
2017-02-20 22:39:47 +0530

    sample test

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Sample Test\n')
+})
+
+app.listen(3000, function () {
+  console.log('Server running at port 3000')
+})
