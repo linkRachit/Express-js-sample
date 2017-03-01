cd8b0677472f5d45e1f616ea64ea702adb06287e
Rachit Goel <rachit2goel@gmail.com>
2017-03-01 23:42:11 +0530

    routes initiated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
-var express = require('express')
+var express = require('express');
+var routes = require('./routes/index');
 
-var app = express()
+var app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Sample Test\n');
-})
+})*/
+
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('Server running at port 3000');
