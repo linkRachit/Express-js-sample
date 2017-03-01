cd8b75756f0771eb3835e15b0054f69ec7e184b2
Rachit Goel <rachit2goel@gmail.com>
2017-03-01 23:26:33 +0530

    use strict

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+'use strict';
+
 var express = require('express')
+
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Sample Test\n')
+  res.send('Sample Test\n');
 })
 
 app.listen(3000, function () {
-  console.log('Server running at port 3000')
+  console.log('Server running at port 3000');
 })
